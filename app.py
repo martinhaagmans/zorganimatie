88c7888b2513c85e95fedafc684b7b252d63f961
Martin Haagmans <mahaagmans@gmail.com>
2018-08-09 14:29:50 +0200

    Added port

--- app.py
+++ app.py
@@ -179,4 +179,5 @@ def upload_filmscript():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)    
